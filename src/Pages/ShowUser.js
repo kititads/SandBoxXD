e792961e6a4de574afb1fc590ad2793d9a2dc082
kititads <76504779+kititads@users.noreply.github.com>
2022-01-27 12:28:15 +0700

    Update ShowUser.js

--- src/Pages/ShowUser.js
+++ src/Pages/ShowUser.js
@@ -9,7 +9,7 @@ function ShowUser()
   return (
         <div className="container Default-Box-Set">
         <div className="text-center" style={{paddingTop : "5%" ,paddingBottom: "15%",textAlign : 'center'}}>
-        <div className="SubBG" ><BiUser size={50} className="icon-set-container"/>All User</div>
+        <div className="SubBG" ><BiUser size={50} className="icon-set-container"/>แสดงผู้ใช้งานทั้งหมด</div>
         <div>
          <div className="">
          <AllUser/>
