5a0b3c6507bbbed7bb0ddb7cdbec8f324b3bdfe9
kititads <76504779+kititads@users.noreply.github.com>
2022-01-27 12:27:59 +0700

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -7,12 +7,12 @@ function NotFound()
   return (
         <div className="container">
         <div className="text-center" style={{margin: "10%",paddingTop : "10%" ,paddingBottom: "10%",textAlign : 'center'}}>
-        <h1><RiErrorWarningLine size={50} className="icon-set-container"/>Not Found Page!</h1>
+        <h1><RiErrorWarningLine size={50} className="icon-set-container"/>ไม่พบหน้านี้!</h1>
 
         <img alt="" src="/img/404.gif" width="50%" height="40%"/>
         <div>
         <a href="/">
-        <Button variant="contained" style={{backgroundColor:'#212F3D',color:'white'}}>Back to home page</Button>{' '}
+        <Button variant="contained" style={{backgroundColor:'#212F3D',color:'white'}}>กลับหน้าหลัก</Button>{' '}
         </a>
 
         </div>
