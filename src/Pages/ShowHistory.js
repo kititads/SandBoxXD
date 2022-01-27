024f5c620355edb1e1850a3148bd4768a86dbc2a
kititads <76504779+kititads@users.noreply.github.com>
2022-01-27 12:28:10 +0700

    Update ShowHistory.js

--- src/Pages/ShowHistory.js
+++ src/Pages/ShowHistory.js
@@ -8,7 +8,7 @@ function ShowHistory()
   return (
         <div className="container Default-Box-Set">
         <div className="text-center" style={{paddingTop : "5%" ,paddingBottom: "15%",textAlign : 'center'}}>
-        <div className="SubBG" ><AiOutlineHistory size={50} className="icon-set-container"/>History</div>
+        <div className="SubBG" ><AiOutlineHistory size={50} className="icon-set-container"/>ประวัติการยืม</div>
         <div>
          <div className="">
          <HistoryTable/>
