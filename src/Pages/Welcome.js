09128ee071022a49711633ad7c72e91d07e87814
kititads <76504779+kititads@users.noreply.github.com>
2022-01-28 01:52:38 +0700

    Update Welcome.js

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,43 +1,23 @@
-
-import React, { useEffect, useState } from "react";
-import * as loadingData from "../loading.json";
-import FadeIn from "react-fade-in";
-import Lottie from "react-lottie";
-
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: loadingData.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
-};
+import React from 'react';
 
 
-function Welcome() {
-  const [loading, setLoading] = useState(false);
+function Welcome() 
+{
+  
 
-  useEffect(() => {
-
-          setLoading(true);
-          
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        
-          <FadeIn>
-            <div style={{ display: "flex" ,opacity: 0.9}}>
-                <Lottie options={defaultOptions} background="#071929" speed="10" height={600} width={600} />
-              
-            </div>
-          </FadeIn>
-       
-  
-      </header>
-    </div>
-  );
+        <div className="container">
+        <div className="text-center" style={{paddingTop : "30%" ,paddingBottom: "30%",textAlign : 'center'}}>
+        <h1>
+       Welcome
+        </h1>
+        </div>
+        </div>
+         );
+         
 }
 
 export default Welcome;
+
+
