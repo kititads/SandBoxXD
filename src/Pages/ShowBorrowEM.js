e9e1c59772ff3781e51c45e803cbcc3f9aef49cd
kititads <76504779+kititads@users.noreply.github.com>
2022-01-27 11:30:03 +0700

    Create ShowBorrowEM.js

---
+++ src/Pages/ShowBorrowEM.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Pages.css';
+
+import { AiOutlineUnorderedList } from "react-icons/ai";
+import BorrowEM from '../Component/BorrowEM';
+
+function ShowBorrowEM() 
+{
+  return (
+        <div className="container Default-Box-Set">
+        <div className="text-center" style={{paddingTop : "5%" ,paddingBottom: "15%",textAlign : 'center'}}>
+        <div className="SubBG" ><AiOutlineUnorderedList size={50} className="icon-set-container"/>Borrow List</div>
+        <div>
+         <div className="">
+         <BorrowEM/>
+         </div>
+         </div>
+        </div>
+        </div>
+       
+        
+         );
+}
+export default ShowBorrowEM;
