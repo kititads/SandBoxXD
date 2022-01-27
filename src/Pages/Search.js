3c86657a7c292117bdad166ff1f17dee81737daf
kititads <76504779+kititads@users.noreply.github.com>
2022-01-27 12:28:03 +0700

    Update Search.js

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -10,7 +10,7 @@ function Search()
   return (
         <div className="container Default-Box-Set">
         <div className="text-center" style={{paddingTop : "5%" ,paddingBottom: "15%",textAlign : 'center'}}>
-        <div className="SubBG" ><AiOutlineSearch size={50} className="icon-set-container"/>Search</div>
+        <div className="SubBG" ><AiOutlineSearch size={50} className="icon-set-container"/>ค้นหา</div>
         <div>
          <div className="">
          <SearchTable/>
