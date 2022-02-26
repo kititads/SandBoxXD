94cae6198818d9e375a80e8554dbc91370eb6571
kititads <76504779+kititads@users.noreply.github.com>
2022-02-26 20:11:30 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCclUTU1dh6fRo5zjI_SxEGK6hB4kpQo8",
-  authDomain: "equipmentdb-97b5d.firebaseapp.com",
-  projectId: "equipmentdb-97b5d",
-  storageBucket: "equipmentdb-97b5d.appspot.com",
-  messagingSenderId: "799840315907",
-  appId: "1:799840315907:web:f49d4c425ee9c542f73c36",
-  measurementId: "G-4MZ8YQV6BM"
+  apiKey: "AIzaSyDaaEGFzrCyAxPaW34Wye80z63edvzgha8",
+  authDomain: "projectnewworld-b14b7.firebaseapp.com",
+  projectId: "projectnewworld-b14b7",
+  storageBucket: "projectnewworld-b14b7.appspot.com",
+  messagingSenderId: "1030911868029",
+  appId: "1:1030911868029:web:6e133a32ffab57b32ec514",
+  measurementId: "G-DRSFP0QQLQ"
 };
 
 // Initialize Firebase
